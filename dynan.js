f29e85b1f78a4ed41888fcc41c19e831222194ab
SCEthan <135861621+SCEthan@users.noreply.github.com>
2025-08-29 19:17:42 +0530

    Update dynan.js

--- dynan.js
+++ dynan.js
@@ -2,8 +2,8 @@ document.addEventListener("mousemove", (e) => {
   const x = e.clientX / window.innerWidth;
   const y = e.clientY / window.innerHeight;
 
-  const offsetX = (0.5 - x) * 40;
-  const offsetY = (0.5 - y) * 40;
+  const offsetX = (0.5 - x) * 10;
+  const offsetY = (0.5 - y) * 10;
 
   document.querySelector("#home_BG").style.backgroundPosition =
     `calc(50% + ${offsetX}px) calc(50% + ${offsetY}px)`;
